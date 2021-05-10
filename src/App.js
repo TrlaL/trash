b347e26524312439d339f367ed85c9e747ee203a
Mark Simkin <marks.simkin@gmail.com>
2021-05-10 23:05:52 +0300

    fix for unviewed updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import './assets/App.css';
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = { isUpdateLogActive: false, updates }
+    this.state = {
+      isUpdateLogActive: false,
+      observer: null,
+      updates: updates,
+      viewedUpdateIds: []
+    }
+  }
+
+  get hasUnviewedUpdates () {
+    return this.state.updates.some(update => {
+      return !this.state.viewedUpdateIds.includes(update.id.toString())
+    })
   }
 
   get updateLogStyle () {
@@ -24,8 +35,40 @@ class App extends React.Component {
     }
   }
 
+  componentWillMount () {
+    this.setState({
+      observer: new IntersectionObserver(this.onIntersectionObserver.bind(this), {
+        root: document.getElementById('list'),
+        rootMargin: '0px',
+        threshold: 1.0
+      }),
+      viewedUpdateIds: JSON.parse(
+        localStorage.getItem('viewedUpdateIds') || '[]'
+      )
+    })
+  }
+
+  componentWillUpdate () {
+    document.querySelectorAll('.update-log__item').forEach(item => {
+      if (!item.isObservable) {
+        this.state.observer.observe(item)
+        return item.isObservable = true
+      }
+    })
+  }
+
   formatDate (date) {
     return new Date(date).toLocaleString()
+  }
+
+  onIntersectionObserver (entries) {
+    entries.forEach(entry => {
+      const id = entry.target.dataset.id
+      if (entry.isIntersecting && !this.state.viewedUpdateIds.includes(id)) {
+        this.state.viewedUpdateIds.push(id)
+        return localStorage.setItem('viewedUpdateIds', JSON.stringify(this.state.viewedUpdateIds))
+      }
+    })
   }
   
   toggleUpdateLogActive () {
@@ -44,6 +87,9 @@ class App extends React.Component {
             className="material-icons update-log__icon"
             onClick={this.toggleUpdateLogActive.bind(this)}>
             notifications
+            {this.hasUnviewedUpdates && (
+              <span className="update-log__icon-label"></span>
+            )}
           </span>
           {this.state.isUpdateLogActive && (
             <div className="update-log__title">
@@ -56,11 +102,19 @@ class App extends React.Component {
             </div>
           )}
         </div>
-        <div className="update-log__list" style={this.updateLogListStyle}>
+        <div
+          className="update-log__list"
+          id="list"
+          style={this.updateLogListStyle}>
           {this.state.updates.map(update => {
             return (
-              <div className="update-log__item" key={update.id}>
-                <div className="update-log__item-title">[{update.version}] {update.title}</div>
+              <div className="update-log__item" data-id={update.id} key={update.id}>
+                <div className="update-log__item-title">
+                  {!this.state.viewedUpdateIds.includes(update.id.toString()) && (
+                    <span className="update-log__item-label"></span>
+                  )}
+                  [{update.version}] {update.title}
+                </div>
                 <div className="update-log__item-date">{this.formatDate(update.date)}</div>
                 <div className="update-log__item-description">{update.description}</div>
                 <button className="update-log__item-button">Try it now</button>
